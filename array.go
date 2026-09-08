a4d7aebd5ec018c550a899ece5be301169986e2b
afakihfaj <afakihfaj@gmail.com>
2026-09-08 12:31:51 +0700

    feat: mempelajari tipe data array

---
+++ array.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var names [3]string
+
+	names[0] = "Afakih"
+	names[1] = "Fajduwani"
+	names[2] = "Goat"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+
+	//array langsung
+
+	var values = [...]int{
+		90,
+		80,
+		95,
+		100,
+		110,
+	}
+	fmt.Println(values)
+	fmt.Println(values[0])
+	fmt.Println(values[1])
+	fmt.Println(values[2])
+
+	fmt.Println(len(values))
+}
