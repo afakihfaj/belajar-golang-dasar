7c29207aafaf336c4beda675ffb682160d8237d2
afakihfaj <afakihfaj@gmail.com>
2026-09-10 11:16:01 +0700

    feat: mempelajari function_value

---
+++ function_value.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func getGoodBye(name string) string {
+	return "Good Bye " + name
+}
+
+func main() {
+	contoh := getGoodBye
+	misal := getGoodBye
+	fmt.Println(contoh("Afakih"))
+	fmt.Println(misal("Afakih"))
+}
