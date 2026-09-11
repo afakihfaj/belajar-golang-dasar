0ac6ac97347ccf2c3dd0e8bd85a02b9fcd505f69
afakihfaj <afakihfaj@gmail.com>
2026-09-11 13:21:50 +0700

    feat: mempelajari panic

--- panic.go
+++ panic.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func endApp() {
 	fmt.Println("End app")
+	message := recover()
+	fmt.Println("terjadi panic", message)
 }
 
 func runApp(error bool) {
@@ -12,9 +14,13 @@ func runApp(error bool) {
 	if error {
 		panic("Ups Error")
 	}
+	// message := recover()
+	// fmt.Println("terjadi panic", message) kalo ditaruh disini,recovernya ga ke panggil,taruh di endapp mas
+
 	//endApp() // kalau panggil endApp() di sini tanpa defer, dia ga bakal jalan karena program keburu mati kena panic duluan
 }
 
 func main() {
 	runApp(true)
+	fmt.Println("Afakih Fajduwani")
 }
