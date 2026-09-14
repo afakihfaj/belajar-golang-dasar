b3a4cc22e795e952421e436c89fcd899c320a8cf
afakihfaj <afakihfaj@gmail.com>
2026-09-14 09:38:06 +0700

    feat: mempelajari package

---
+++ helper/helper.go
@@ -0,0 +1,20 @@
+package helper
+
+import "fmt"
+
+var version = "1.0.0"
+var Application = "golang"
+
+func sayGoodBye(name string) string {
+	return "Good Bye " + name
+}
+
+func SayHello(name string) string {
+	return "Hello " + name
+}
+
+func Contoh() {
+	sayGoodBye("Afakih")
+	fmt.Println(version)
+
+}
