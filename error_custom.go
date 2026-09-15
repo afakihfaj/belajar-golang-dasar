5fcd65b2c627676f843435abe47b536332ce77f0
afakihfaj <afakihfaj@gmail.com>
2026-09-15 10:49:26 +0700

    feat: mempelajari custom error

---
+++ error_custom.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+type validationError struct {
+	Message string
+}
+
+func (v *validationError) Error() string {
+	return v.Message
+}
+
+type notFoundError struct {
+	Message string
+}
+
+func (n *notFoundError) Error() string {
+	return n.Message
+}
+
+func SaveData(id string, data any) error {
+	if id == "" {
+		return &validationError{Message: "Validation error"}
+	}
+
+	if id != "afakih" {
+		return &notFoundError{Message: "data not found"}
+	}
+	return nil
+}
+
+func main() {
+	err := SaveData("afakih", nil)
+	if err != nil {
+		// //terjadi error
+		// if validationErr, ok := err.(*validationError); ok {
+		// 	fmt.Println("validation error:", validationErr.Error())
+		// } else if notFoundErr, ok := err.(*notFoundError); ok {
+		// 	fmt.Println("not found error:", notFoundErr.Error())
+		// } else {
+		// 	fmt.Println("Unknown error:", err.Error())
+		// }
+
+		switch finalError := err.(type) {
+		case *validationError:
+			fmt.Println("validation error:", finalError.Error())
+		case *notFoundError:
+			fmt.Println("not found error:", finalError.Error())
+		default:
+			fmt.Println("unknown error:", finalError.Error())
+		}
+
+	} else {
+		//suksess
+		fmt.Println("Sukses")
+	}
+}
